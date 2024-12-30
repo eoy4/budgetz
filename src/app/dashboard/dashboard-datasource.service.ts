f911cb5adbe62eb23c9d8c6eb0e623bf3ea9740d
Ricardo Tovar <ricardo.tovar@gmail.com>
2024-12-29 17:28:51 -0800

    add dashboard datasource service

---
+++ src/app/dashboard/dashboard-datasource.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { transactions, settings } from '../../dummy-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardDatasourceService {
+
+  constructor() { }
+}
